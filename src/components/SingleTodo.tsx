0b3e8a2e0299321a4c7675bc7405df738ab28f71
serii burduja <seriiburduja@gmail.com>
2026-06-23 13:02:37 +0300

    feat: handle edit and delete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -9,18 +9,30 @@ interface Props {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-export const SingleTodo: React.FC<Props> = ({ todo }) => {
+export const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  function handleDone(id: number) {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+  }
+
+  function handleDelete(id: number) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <form className="todos__single">
-      <span className="todos__single-text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single-text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single-text">{todo.todo}</span>
+      )}
       <div>
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
