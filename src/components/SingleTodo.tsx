5cfb2e57a47718e8fda71e438d11618c697f0070
serii burduja <seriiburduja@gmail.com>
2026-06-23 13:18:37 +0300

    feat: edited todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Todo } from "../models";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -10,6 +10,9 @@ interface Props {
 }
 
 export const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedTodo, setEditedTodo] = useState(todo.todo);
+
   function handleDone(id: number) {
     setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
   }
@@ -18,17 +21,39 @@ export const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
+  function handleEdit() {
+    if (!isEdit && !todo.isDone) {
+      setIsEdit(true);
+    }
+  }
+
+  function handleBlur() {
+    setIsEdit(false);
+    setTodos(todos.map((t) => (t.id === todo.id ? { ...t, todo: editedTodo } : t)));
+  }
+
   return (
     <form className="todos__single">
       {todo.isDone ? (
         <s className="todos__single-text">{todo.todo}</s>
+      ) : isEdit ? (
+        <input
+          type="text"
+          className="todos__single-text"
+          name="edit"
+          value={editedTodo}
+          onChange={(e) => setEditedTodo(e.target.value)}
+          onBlur={handleBlur}
+        />
       ) : (
         <span className="todos__single-text">{todo.todo}</span>
       )}
-      <div>
-        <span className="icon">
-          <AiFillEdit />
-        </span>
+      <div className="todos__wrap">
+        {!todo.isDone && (
+          <span className="icon" onClick={handleEdit}>
+            <AiFillEdit />
+          </span>
+        )}
         <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
