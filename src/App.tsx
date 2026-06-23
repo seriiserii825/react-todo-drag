06a1d37c35a6daab8fa82497fd6e8800ef55297d
seriiserii825 <seriiburduja@gmail.com>
2026-06-23 20:12:46 +0300

    feat: reducer in another file

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,11 @@ import type React from "react";
 import { useReducer, useState } from "react";
 import InputField from "./components/InputField";
 import { TodoList } from "./components/TodoList";
-import type { Todo } from "./models";
+import { todoReducer } from "./reducers/todoReducer";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, dispatch] = useReducer(todoReducer, []);
-
-  type Actions =
-    | { type: "add"; payload: string }
-    | { type: "delete"; payload: number }
-    | { type: "edit"; payload: { id: number; todo: string } }
-    | { type: "toggleDone"; payload: number };
-
-  function todoReducer(state: Todo[], action: Actions): Todo[] {
-    switch (action.type) {
-      case "add":
-        return [...state, { id: Date.now(), todo: action.payload, isDone: false }];
-      case "delete":
-        return state.filter((t) => t.id !== action.payload);
-      case "edit":
-        return state.map((t) =>
-          t.id === action.payload.id ? { ...t, todo: action.payload.todo } : t,
-        );
-      case "toggleDone":
-        return state.map((t) => (t.id === action.payload ? { ...t, isDone: !t.isDone } : t));
-      default:
-        return state;
-    }
-  }
 
   function handleAdd(e: React.SubmitEvent) {
     e.preventDefault();
---
+++ src/reducers/todoReducer.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../models";
+import type { Actions } from "../types/Actions";
+
+export function todoReducer(state: Todo[], action: Actions): Todo[] {
+  switch (action.type) {
+    case "add":
+      return [...state, { id: Date.now(), todo: action.payload, isDone: false }];
+    case "delete":
+      return state.filter((t) => t.id !== action.payload);
+    case "edit":
+      return state.map((t) =>
+        t.id === action.payload.id ? { ...t, todo: action.payload.todo } : t,
+      );
+    case "toggleDone":
+      return state.map((t) => (t.id === action.payload ? { ...t, isDone: !t.isDone } : t));
+    default:
+      return state;
+  }
+}
