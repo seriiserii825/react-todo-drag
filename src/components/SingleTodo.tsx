ae415114eb5d9a222a83f9e1c0e94fbcad4d80e4
serii burduja <seriiburduja@gmail.com>
2026-06-23 12:51:24 +0300

    feat: styled single todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { Todo } from "../models";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 interface Props {
   todo: Todo;
@@ -12,9 +14,15 @@ export const SingleTodo: React.FC<Props> = ({ todo }) => {
     <form className="todos__single">
       <span className="todos__single-text">{todo.todo}</span>
       <div>
-        <span className="icon"></span>
-        <span className="icon"></span>
-        <span className="icon"></span>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
       </div>
     </form>
   );
